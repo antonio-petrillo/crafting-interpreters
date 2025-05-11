80c927d0b60a688c45a6c3c277d3c161fe1bf6e2
= <=>
2025-05-11 23:04:38 +0200

    first commit

---
+++ tree_walker/lox/src/main/java/com/craftinginterpreters/lox/Main.java
@@ -0,0 +1,10 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
