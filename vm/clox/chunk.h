3a676c942ba06447c353cc817847805e1b78bfc3
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-01-02 14:59:38 +0100

    chunks of bytecode

---
+++ vm/clox/chunk.h
@@ -0,0 +1,25 @@
+#ifndef CLOX_CHUNK_H
+#define CLOX_CHUNK_H
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OP_RETURN,
+  OP_CONSTANT,
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t *code;
+  ValueArray constants;
+  int *lines;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+int addConstant(Chunk *chunk, Value value);
+
+#endif
