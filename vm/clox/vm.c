bc38c8be5d7213ecdb56c3d241aa9a231a47dfa8
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-01-11 20:41:44 +0100

    Scanning on Demand

--- vm/clox/vm.c
+++ vm/clox/vm.c
@@ -1,6 +1,7 @@
 #include "common.h"
-#include "vm.h"
+#include "compiler.h"
 #include "debug.h"
+#include "vm.h"
 
 #include <stdio.h>
 
@@ -63,10 +64,9 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
 
 void push(Value value) {
