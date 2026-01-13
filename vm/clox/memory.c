7763c2a64fffce9dce3c8125be7ee08cd96f3017
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-01-13 16:10:57 +0100

    Calls and Functions

--- vm/clox/memory.c
+++ vm/clox/memory.c
@@ -21,6 +21,14 @@ static void freeObject(Obj *object) {
     FREE_ARRAY(char, string->chars, string->length + 1);
     FREE(ObjString, object);
   } break;
+  case OBJ_FUNCTION: {
+    ObjFunction *function = (ObjFunction *) object;
+    freeChunk(&function->chunk);
+    FREE(ObjFunction, object);
+  } break;
+  case OBJ_NATIVE: {
+    FREE(ObjNative, object);
+  } break;
   }
 }
 
