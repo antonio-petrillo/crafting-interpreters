c6873a56c363be7ea1524ef406a077a3c3316ced
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-01-16 23:12:10 +0100

    closures

--- vm/clox/memory.c
+++ vm/clox/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "object.h"
 #include "vm.h"
 
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
@@ -21,10 +22,18 @@ static void freeObject(Obj *object) {
     FREE_ARRAY(char, string->chars, string->length + 1);
     FREE(ObjString, object);
   } break;
+  case OBJ_CLOSURE: {
+    ObjClosure *closure = (ObjClosure *) object;
+    FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
+    FREE(ObjClosure, object);
+  } break;
   case OBJ_FUNCTION: {
     ObjFunction *function = (ObjFunction *) object;
     freeChunk(&function->chunk);
     FREE(ObjFunction, object);
+  } break;
+  case OBJ_UPVALUE: {
+    FREE(ObjUpvalue, object) ;
   } break;
   case OBJ_NATIVE: {
     FREE(ObjNative, object);
