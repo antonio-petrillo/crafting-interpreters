d48b6541b893bce32a35a6a8aec916daa7e5cf41
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-01-17 10:40:00 +0100

    methods and initializers

--- vm/clox/object.h
+++ vm/clox/object.h
@@ -7,6 +7,7 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_BOUND_METHOD(value) isObjType(value, OBJ_BOUND_METHOD)
 #define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
@@ -14,6 +15,7 @@
 #define IS_CLASS(value) isObjType(value, OBJ_CLASS)
 #define IS_INSTANCE(value) isObjType(value, OBJ_INSTANCE)
 
+#define AS_BOUND_METHOD(value) ((ObjBoundMethod *) AS_OBJ(value))
 #define AS_INSTANCE(value) ((ObjInstance *) AS_OBJ(value))
 #define AS_CLASS(value) ((ObjClass *) AS_OBJ(value))
 #define AS_CLOSURE(value) ((ObjClosure *) AS_OBJ(value))
@@ -30,6 +32,7 @@ typedef enum {
   OBJ_UPVALUE,
   OBJ_CLASS,
   OBJ_INSTANCE,
+  OBJ_BOUND_METHOD,
 } ObjType;
 
 struct Obj {
@@ -77,6 +80,7 @@ typedef struct {
 typedef struct {
   Obj obj;
   ObjString *name;
+  Table methods;
 } ObjClass;
 
 typedef struct {
@@ -85,6 +89,13 @@ typedef struct {
   Table fields;
 } ObjInstance;
 
+typedef struct {
+  Obj obj;
+  Value receiver;
+  ObjClosure *method;
+} ObjBoundMethod;
+
+ObjBoundMethod *newBoundMethod(Value receiver, ObjClosure *method);
 ObjInstance *newInstance(ObjClass *klass);
 ObjClass *newClass(ObjString *name);
 ObjClosure *newClosure(ObjFunction *function);
